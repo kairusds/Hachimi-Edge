5e54789cd20d2249d1406ae552c330754217139b
LeadRDRK <leadrdrk@zohomail.com>
2025-06-19 21:11:53 +0700

    [windows] Revert symbol resolver to Unity 2020.3

--- src/windows/symbols_impl.rs
+++ src/windows/symbols_impl.rs
@@ -34,7 +34,6 @@ const SYMBOL_LIST: &[&'static str] = &[
     "il2cpp_assembly_get_image",
     "il2cpp_class_for_each",
     "il2cpp_class_enum_basetype",
-    "il2cpp_class_is_inited",
     "il2cpp_class_is_generic",
     "il2cpp_class_is_inflated",
     "il2cpp_class_is_assignable_from",
@@ -124,8 +123,6 @@ const SYMBOL_LIST: &[&'static str] = &[
     "il2cpp_gc_foreach_heap",
     "il2cpp_stop_gc_world",
     "il2cpp_start_gc_world",
-    "il2cpp_gc_alloc_fixed",
-    "il2cpp_gc_free_fixed",
     "il2cpp_gchandle_new",
     "il2cpp_gchandle_new_weakref",
     "il2cpp_gchandle_get_target",
@@ -136,11 +133,10 @@ const SYMBOL_LIST: &[&'static str] = &[
     "il2cpp_offset_of_array_length_in_array_object_header",
     "il2cpp_offset_of_array_bounds_in_array_object_header",
     "il2cpp_allocation_granularity",
-    "il2cpp_unity_liveness_allocate_struct",
+    "il2cpp_unity_liveness_calculation_begin",
+    "il2cpp_unity_liveness_calculation_end",
     "il2cpp_unity_liveness_calculation_from_root",
     "il2cpp_unity_liveness_calculation_from_statics",
-    "il2cpp_unity_liveness_finalize",
-    "il2cpp_unity_liveness_free_struct",
     "il2cpp_method_get_return_type",
     "il2cpp_method_get_declaring_type",
     "il2cpp_method_get_name",
@@ -210,7 +206,6 @@ const SYMBOL_LIST: &[&'static str] = &[
     "il2cpp_type_get_attrs",
     "il2cpp_type_equals",
     "il2cpp_type_get_assembly_qualified_name",
-    "il2cpp_type_get_reflection_name",
     "il2cpp_type_is_static",
     "il2cpp_type_is_pointer_type",
     "il2cpp_image_get_assembly",
@@ -230,20 +225,18 @@ const SYMBOL_LIST: &[&'static str] = &[
     "il2cpp_unity_install_unitytls_interface",
     "il2cpp_custom_attrs_from_class",
     "il2cpp_custom_attrs_from_method",
-    "il2cpp_custom_attrs_from_field",
     "il2cpp_custom_attrs_get_attr",
     "il2cpp_custom_attrs_has_attr",
     "il2cpp_custom_attrs_construct",
     "il2cpp_custom_attrs_free",
     "il2cpp_class_set_userdata",
     "il2cpp_class_get_userdata_offset",
-    "il2cpp_set_default_thread_affinity",
-    "il2cpp_unity_set_android_network_up_state_func"
+    "il2cpp_set_default_thread_affinity"
 ];
 
-const START_RVA: u32 = 0x7834a2;
+const START_RVA: u32 = 0x8c6714;
 fn generate_symbol_map() -> Result<FnvHashMap<&'static str, CString>, Error> {
-    let mut map = FnvHashMap::with_capacity(SYMBOL_LIST.len());
+    let mut map = FnvHashMap::with_capacity(225);
 
     let mut path = utils::get_game_dir().unwrap();
     path.push("UnityPlayer.dll");
@@ -258,7 +251,7 @@ fn generate_symbol_map() -> Result<FnvHashMap<&'static str, CString>, Error> {
         let name_offset = pe.rva_to_file_offset(rva + 0x4 + rip_offset)?;
         let name = unsafe { CStr::from_ptr(image[name_offset..].as_ptr() as _) };
         map.insert(*symbol, name.to_owned());
-        rva += if rva == START_RVA { 0x28 } else { 0x26 };
+        rva += if rva == START_RVA { 0x35 } else { 0x30 };
     }
 
     Ok(map)
